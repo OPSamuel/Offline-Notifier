b3f9075b6b8095ce8a6464a07429c76d02ce0801
OP SamuelCC <80846151+Sammmmmmmmmmmmy@users.noreply.github.com>
2024-05-26 22:32:42 +0100

    Update information.js

--- src/commands/Other/information.js
+++ src/commands/Other/information.js
@@ -10,7 +10,7 @@ module.exports = {
         const embed = new EmbedBuilder()
         .setColor("Green")
         .setTitle("Bot Information")
-        .setDescription("Hey, I am Offline Notifier. My main role is to work alongside <@994504651565183007>, if it goes down or has any issues I will be there to let everyone know.\n\nI am a developer only bot meaning you can't have me, and that all of my commands are developer only unfortunately.")
+        .setDescription("Hey, I am Offline Notifier. All of my commands are owner only unfortunately.")
 
         interaction.reply({embeds: [embed]})
     }
